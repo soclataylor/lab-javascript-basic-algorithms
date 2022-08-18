7796571b9efd9eca7370e0dc9d1fbf5b1233b3c8
soclataylor <mariona.batalla.taylor@gmail.com>
2022-08-18 21:31:15 +0200

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
+/*
+            MARIONA BATALLA TAYLOR
+            mariona.batalla.taylor@gmail.com
+            IronHack - FrontEnd Bootcamp
+            Week 3 - Day 1 Lab: JAVASCRIPT BASIC ALGORITHMS
+            August 2022
+*/
+
+
 // Iteration 1: Names and Input
-//
+/*
+
+1.1 Create a variable hacker1 with the driver's name.
+1.2 Print "The driver's name is XXXX".
+1.3 Create a variable hacker2 with the navigator's name.
+1.4 Print "The navigator's name is YYYY".
+*/
+
+
+
 // Iteration 2: Conditionals
 
 
